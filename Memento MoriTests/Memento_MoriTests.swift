35a537ec3d0cfba97ff35a9e03275ba9e798b499
Chetan Singh <heychetansingh@gmail.com>
2025-05-01 16:27:06 +0530

    Initial Commit

---
+++ Memento MoriTests/Memento_MoriTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memento_MoriTests.swift
+//  Memento MoriTests
+//
+//  Created by Chetan Singh on 2025-05-01.
+//
+
+import Testing
+@testable import Memento_Mori
+
+struct Memento_MoriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
